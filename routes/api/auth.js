7b072fc4fba23a56c2b7f11de8618e36a2c621d5
arghavan0121 <arghavan0121r@gmail.com>
2023-12-25 11:02:03 +0330

    Add login routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,16 +3,23 @@ const router = express.Router();
 
 // controller
 const registerController = require('./../../app/Http/Controllers/Auth/registerController');
-const verifyController = require('../../app/Http/Controllers/Auth/verifyController');
+const verifyController = require('app/Http/Controllers/Auth/verifyController');
+const loginController = require('app/Http/Controllers/Auth/loginController');
 
 
 //validator
 const registerValidator = require('./../../app/Http/Validators/registerValidator');
 const verifyValidator = require('./../../app/Http/Validators/verifyValidator');
+const loginValidator = require('./../../app/Http/Validators/loginValidator');
 
 // register
 router.get('/register', registerController.showForm);
 router.post('/register', registerValidator.handle() , registerController.registerProcess);
+
+
+//login
+router.get('/login', loginController.showForm);
+router.post('/login', loginValidator.handle(), loginController.loginProcess);
 
 //verify Code
 router.get('/verify', verifyController.showForm);
